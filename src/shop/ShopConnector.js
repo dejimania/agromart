aa8aa72d77c05d378dd0359922df850735ae609d
Kamil Balogun <kamilbalogun1@gmail.com>
2020-06-06 14:55:01 +0100

    feat(add): connect to data store and url router

---
+++ src/shop/ShopConnector.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Switch, Redirect, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { loadData } from '../data/ActionCreators';
+import { DataTypes } from '../data/Types';
+import { Shop } from './Shop';
+
+const mapStateToProps = (dataStore) => ({
+  ...dataStore
+});
+
+const mapDispatchToProps = {
+  loadData
+};
+
+const filterProducts = (products = [], category) => {
+  (!category || category === "All")
+  ? products
+  : products.filter((p) => { p.category.toLowerCase() === category.toLowerCase() });
+}
+
+export const ShooConnector = connect(mapStateToProps, mapDispatchToProps)(
+  class extends Component {
+
+    componentDidMount() {
+      this.props.loadData(DataTypes.CATEGORIES);
+      this.props.loadData(DataTypes.PRODUCTS);
+    }
+
+    render() {
+      return (
+        <Switch>
+          <Route path="/shop/products:category?"
+            render={ (routeProp) => {
+              <Shop { ...this.props } { ...routeProp }
+                products={ filterProducts(this.props.products,
+                  routeProp.match.params.category)}
+              />
+            }}
+          />
+          <Redirect to="/shop/products" />
+        </Switch>
+      );
+    }
+  }
+)
