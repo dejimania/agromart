2e6ef894c9caad84cc1deb4b27315806904a3569
Kamil Balogun <kamilbalogun1@gmail.com>
2020-06-07 15:02:13 +0100

    feat(common-reducer): Add CommonReducer

--- src/data/DataStore.js
+++ src/data/DataStore.js
@@ -1,4 +1,6 @@
 import { createStore } from 'redux';
 import { MartReducer } from './MartReducer';
+import { CartReducer } from './CartReducer';
+import { CommonReducer } from './CommonReducer';
 
-export const AgroMartDataStore = createStore(MartReducer);
+export const AgroMartDataStore = createStore(CommonReducer(MartReducer, CartReducer));
