c7ddfef28cc6b7149de70d64445eb9e565b08652
Kamil Balogun <kamilbalogun1@gmail.com>
2020-06-12 13:38:07 +0100

    feat: Add post method to RestDataSource

--- src/data/RestDataSource.js
+++ src/data/RestDataSource.js
@@ -2,10 +2,19 @@ import Axios from 'axios';
 import { RestUrls } from './Urls'
 
 export class RestDataSource {
+  constructor(err_handler) {
+    this.err_handler = err_handler || (() => {});
+  }
 
   getData = (dataType, params) => {
     return this.sendRequest("get", RestUrls[dataType], params);
   }
 
-  sendRequest = (method, url, params) => Axios.request({ method, url, params });
+  StoreData = (dataType, data) => {
+    return this.sendRequest("post", RestUrls[dataType], {}, data);
+  }
+
+  sendRequest = (method, url, params, data) => Axios.request({
+    method, url, params, data 
+  });
 }
