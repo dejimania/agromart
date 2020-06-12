d51da2a1fb5598b3daac6abdcb458b11946dcd47
Kamil Balogun <kamilbalogun1@gmail.com>
2020-06-12 16:03:06 +0100

    chore: alter routing config in ShopConnector

--- src/shop/ShopConnector.js
+++ src/shop/ShopConnector.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Switch, Redirect, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { loadData } from '../data/ActionCreators';
+import { loadData, placeOrder } from '../data/ActionCreators';
 import { DataTypes } from '../data/Types';
 import { Shop } from '../shop/Shop';
 import { addToCart, updateCartQuantity, removeFromCart, clearCart } from '../data/CartActionCreators';
 import { CartDetails } from './CartDetails';
 import { DataGetter } from '../data/DataGetter';
+import { Checkout } from './Checkout';
+import { Thanks } from './Thanks';
 
 const mapStateToProps = (dataStore) => ({
   ...dataStore
@@ -17,7 +19,8 @@ const mapDispatchToProps = {
   addToCart,
   updateCartQuantity,
   removeFromCart,
-  clearCart
+  clearCart,
+  placeOrder
 };
 
 // const filterProducts = (products = [], category) =>
@@ -49,6 +52,16 @@ export const ShopConnector = connect(mapStateToProps, mapDispatchToProps)(
             render={ (routeProps) => {
               return <CartDetails { ...this.props} { ...routeProps }/>
             } }
+          />
+          <Route path="/shop/checkout"
+            render={ (routeProps) => {
+              return <Checkout { ...this.props } { ...routeProps }/>
+            }}
+          />
+          <Route path="/shop/thanks"
+            render={ (routeProps) => {
+              return <Thanks { ...this.props } { ...routeProps }/>
+            }}
           />
           <Redirect to="/shop/products/all/1" />
         </Switch>
